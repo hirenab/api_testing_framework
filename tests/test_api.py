3c74bc9e68e531f161515d7b6c64a7f6123f14d3
Hiren Alphabin <hiren.alphabin@gmail.com>
2025-03-19 11:15:43 +0530

    fix the error

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,11 +2,12 @@
 import pytest
 from helpers.base_functions import *
 from helpers.payloads import * 
+
+from helpers import *
 # Importing payload data for creating and updating posts
 
 create_post_payload = create_post_payload
 update_post_payload = update_post_payload
-# from api_testing_framework.helpers
 # Test function for GET request to retrieve a post
 @pytest.mark.get_request
 def test_get_post():
